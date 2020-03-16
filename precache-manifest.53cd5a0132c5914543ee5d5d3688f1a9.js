f90f2e0a9b7f7dc9b5cb788b18b771735f13fdd1
mustafa <mustafa.ahmad@3lmdigital.com>
2020-03-16 16:48:22 +0200

    Updates

---
+++ precache-manifest.53cd5a0132c5914543ee5d5d3688f1a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b865152c652d83cfbf7ff75297fef8ff",
+    "url": "/hooks-app/index.html"
+  },
+  {
+    "revision": "56eba83b4744fb3734f7",
+    "url": "/hooks-app/static/css/main.acf4bcdb.chunk.css"
+  },
+  {
+    "revision": "ebb63c26adbf678ac0fa",
+    "url": "/hooks-app/static/js/2.8c72348c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/hooks-app/static/js/2.8c72348c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56eba83b4744fb3734f7",
+    "url": "/hooks-app/static/js/main.003844cb.chunk.js"
+  },
+  {
+    "revision": "bdd2a93b4ab2d8573960",
+    "url": "/hooks-app/static/js/runtime-main.ffe0d40e.js"
+  }
+]);
